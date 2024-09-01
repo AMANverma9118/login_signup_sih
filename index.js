69e4d6363772afa8772d0b4574186f2de734a054
AMANverma9118 <AMANverma9118359330>
2024-09-02 00:31:49 +0530

    Index is ready

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const routes = require("./Routes/route")
+const app = express();
+const appRoute = require("./Routes/route");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT||5000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use(express.json());
+
+
+const connectDB = require('./Mongoose');
+connectDB()
+
+app.use('/',appRoute);
+
+app.get('/',(req,res)=>{
+    res.send('Hello everyone, Your name');
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is runing on http://localhost:${PORT}`)
+})
