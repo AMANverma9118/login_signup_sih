efbbb5a5afd6bd3e1b0b4d01cb4dd620ee0a6636
AMANverma9118 <AMANverma9118359330>
2024-09-02 03:38:23 +0530

    Deploy

--- Mongoose.js
+++ Mongoose.js
@@ -5,8 +5,6 @@ mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB successfully");
   } catch (error) {
